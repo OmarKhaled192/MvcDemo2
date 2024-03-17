2572bda03a9acf8f00324888c87eebcda0b46751
omarkhaled <okoros192000@gmail.com>
2024-03-17 09:13:28 +0200

    new update

---
+++ demo/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
